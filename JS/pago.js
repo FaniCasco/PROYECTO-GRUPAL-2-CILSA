b575c571e9f95f30e37d49b6a4292b9a576a3432
Fani Casco <fanymar@live.com>
2024-10-02 10:27:56 -0300

    sweet alert de pagos ajustado

--- JS/pago.js
+++ JS/pago.js
@@ -2,15 +2,6 @@
 const cart = JSON.parse(localStorage.getItem('cart')) || [];
 let total = cart.reduce((sum, item) => sum + item.price, 0);
 
-// Función para mostrar el mensaje de pago exitoso
-function showPaymentMessage(message) {
-    const paymentMessageDiv = document.getElementById('payment-message');
-    paymentMessageDiv.textContent = message;
-    paymentMessageDiv.style.padding = '20px';
-    paymentMessageDiv.style.border = '1px solid green';
-    paymentMessageDiv.style.marginTop = '20px';
-    paymentMessageDiv.style.textAlign = 'center';
-}
 
 // Función para vaciar el carrito
 function clearCart() {
@@ -22,31 +13,32 @@ function showPaymentMessage(message, icon) {
     Swal.fire({
         title: '¡Pedido confirmado!',
         text: message,
-        icon: icon,  // Puedes usar 'success', 'info', 'warning', etc.
-        background: '#fff4e0',  // Un tono cálido para comida rápida
-        confirmButtonColor: '#e94e77',  // Un rojo vibrante para simular ketchup
-        timer: 3000,
+        icon: icon,
+        background: '#fff4e0',
+        confirmButtonColor: '#e94e77',
+        timer: 5000,
         timerProgressBar: true,
-        backdrop: `
-            rgba(0,0,123,0.4)
-            url("/IMG/hamburguesa.gif")  // Añade una imagen de fondo divertida relacionada
-            left top
-            no-repeat
-        `
+        width: '40rem', 
+        customClass: {
+            popup: 'swal2-popup',  // Clase personalizada para el popup
+           
+        }
     });
 }
 
+
+
 // Pagar con tarjeta
 document.getElementById('card-payment').addEventListener('click', () => {
-    showPaymentMessage("Pago realizado con éxito. ¡Tu pedido está en camino!", 'success');
+    showPaymentMessage(`Pago realizado con éxito. ¡Tu pedido está en camino!`, 'success');
     clearCart();
-    setTimeout(() => window.location.href = 'index.html', 3000);
+    setTimeout(() => window.location.href = 'index.html', 5000);
 });
 
 // Pagar en efectivo
 document.getElementById('cash-payment').addEventListener('click', () => {
-    showPaymentMessage(`Gracias por tu compra. Deberás pagar al repartidor $${total}.`, 'info');
+    showPaymentMessage(`Gracias por tu compra. Deberás pagar al repartidor $${total}.`, 'success');
     clearCart();
-    setTimeout(() => window.location.href = 'index.html', 3000);
+    setTimeout(() => window.location.href = 'index.html', 5000);
 });
 
